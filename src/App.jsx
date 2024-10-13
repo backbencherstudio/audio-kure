012c8cc3aa8bebdff9f2f3b8a970e429ef3b80bf
sohanur <sr.sohan088@gmail.com>
2024-10-13 11:03:08 +0600

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from 'react';
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increase Count</button>
-      
-      <audio controls>
-        <source src="https://www.dropbox.com/scl/fi/n7n5swrmwc7j566ma6kdb/file_example_MP3_5MG.mp3?rlkey=tckycf4ciob0tfdyzrspwo80z&st=4prulsix&dl=1" type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
-z
 export default App;
