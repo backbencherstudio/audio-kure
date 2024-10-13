dd5280e77bc78b22308145b27037d0d8e0fc8f45
hasib1010 <asifmahmud664@gmail.com>
2024-10-13 15:46:48 +0600

    landing page added

--- src/shared/ProgressBar/ProgressBar.jsx
+++ src/shared/ProgressBar/ProgressBar.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ProgressBar from '@ramonak/react-progress-bar';
-
+import logo from "./../../assets/images/logo.png"
+import { GrLinkNext, GrLinkPrevious } from 'react-icons/gr';
+import { Link } from 'react-router-dom';
 const ProgressBars = ({ value }) => {
     return (
         <div>
-            <ProgressBar completed={value ? value : 10} ></ProgressBar>
+            <div className='flex items-center justify-evenly'>
+                <div>
+                    <Link className='flex items-center gap-5'><GrLinkPrevious ></GrLinkPrevious > Back</Link>
+                </div>
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+                <div> </div>
+            </div>
+            <ProgressBar className='bg-transparent'
+                completed={value ? value : 10}
+                labelColor="transparent"
+                labelAlignment="center"
+            />
         </div>
     );
 }
