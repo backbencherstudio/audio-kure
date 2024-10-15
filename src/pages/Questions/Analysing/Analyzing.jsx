ca86915529d1d27b055f9d4ce396e922c255fa86
Backbencher Studio <sr.sohan088@gmail.com>
2024-10-15 15:30:45 +0600

    merge hasib

--- src/pages/Questions/Analysing/Analyzing.jsx
+++ src/pages/Questions/Analysing/Analyzing.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import './Analyzing.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./Analyzing.css";
+import { useNavigate } from "react-router-dom";
 
 const Analyzing = () => {
   const navigate = useNavigate();
@@ -8,7 +8,7 @@ const Analyzing = () => {
   const steps = [
     "Analyzing your answers",
     "Calculating your weight loss forecast",
-    "Creating your personalized hypnosis program"
+    "Creating your personalized hypnosis program",
   ];
 
   useEffect(() => {
@@ -28,33 +28,41 @@ const Analyzing = () => {
 
   useEffect(() => {
     if (progress === 100) {
-
-      // setTimeout(() => navigate("/welcome"), 300);
+      setTimeout(() => navigate("/welcome"), 300);
     }
   }, [progress, navigate]);
 
-
-  const activeStep = Math.min(Math.floor((progress / 100) * steps.length), steps.length - 1);
+  const activeStep = Math.min(
+    Math.floor((progress / 100) * steps.length),
+    steps.length - 1
+  );
 
   return (
-    <div className='flex items-center justify-center lg:pt-20 pt-5 mx-5 '>
+    <div className="flex items-center justify-center lg:pt-20 pt-5 mx-5 ">
       <div className="loading-container lg:w-1/3 p-2 lg:p-20">
-        <h1 className='text-3xl my-3 mb-4'>All set! Just a moment while we process your data...</h1>
+        <h1 className="text-3xl my-3 mb-4">
+          All set! Just a moment while we process your data...
+        </h1>
         <div className="progress-bar-container">
           <div className="progress-bar" style={{ width: `${progress}%` }} />
         </div>
         <div className="steps">
           {steps.map((step, index) => (
-            <div key={index} className={`step ${index <= activeStep ? 'completed' : ''}`}>
-              {index <= activeStep ? <span>&#10003;</span> : <span>•••</span>} {step}
+            <div
+              key={index}
+              className={`step ${index <= activeStep ? "completed" : ""}`}
+            >
+              {index <= activeStep ? <span>&#10003;</span> : <span>•••</span>}{" "}
+              {step}
             </div>
           ))}
         </div>
         <div className="testimonial">
           <div className="stars">★★★★★</div>
           <p>
-            "It's the easiest weight loss solution I've ever tried. Evening sessions have greatly
-            enhanced my sleep quality and significantly reduced my stress."
+            "It's the easiest weight loss solution I've ever tried. Evening
+            sessions have greatly enhanced my sleep quality and significantly
+            reduced my stress."
           </p>
           <p className="author">- Laura K.</p>
           <p className="verified">VERIFIED USER</p>
